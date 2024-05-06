835485fdf0eeb7f66813db2fd3977aacf942b35e
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-06 10:26:29 +0530

    Profile Upload routes added.

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const crypto = require("crypto");
 const path = require("path");
+// const multerconfig = require("./config/multerConfig");
+const upload = require("./config/multerConfig");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -17,6 +21,17 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/profile/upload", (req, res) => {
+  res.render("profileupload");
+});
+
+app.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.profile = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
 });
 
 app.get("/login", (req, res) => {
